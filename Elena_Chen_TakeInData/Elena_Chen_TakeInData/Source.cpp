0dcabf143e6be7b88e4ca59e4e583fe3abbf0f48
elenayuechen <31861618+elenayuechen@users.noreply.github.com>
2017-09-27 12:30:17 -0500

    Add files via upload
    This is the correct upload.

--- Elena_Chen_TakeInData/Elena_Chen_TakeInData/Source.cpp
+++ Elena_Chen_TakeInData/Elena_Chen_TakeInData/Source.cpp
@@ -23,6 +23,7 @@ void main() {
 	char teacher; // what's you english teacher's lastname initial? answer with a capital leter
 	bool favor; // do you like english? 1=yes 0=no
 	double price; // how much does a bottle of water cost? 
+	int calc_1, calc_2, calc_3, calc_4;
 	//user queries
 	cout << "what grade level are you in? answer w/ a number";
 	cin >> gradelevel;
@@ -34,11 +35,23 @@ void main() {
 	cin >> favor;
 	cout << "what is the price of your water bottle?";
 	cin >> price;
+    cout << "enter a number : ";
+	cin >> calc_1;
+	cout << "enter a number : ";
+	cin >> calc_2;
+	cout << "enter a number : ";
+	cin >> calc_3;
+	cout << "enter a number : ";
+	cin >> calc_4;
 	//print out data
-	cout << "your are in grade " << gradelevel;
-	cout << "you have a " << grade << "in english";
-	cout << "your teacher's last name starts with " << teacher;
-	cout << "your favor towards enlish is " << favor;
-	cout << "the price of your water bottle is" << price;
+	cout << "you are in grade " << gradelevel << endl;
+	cout << "you have a " << grade << "in english" << endl;
+	cout << "your teacher's last name starts with " << teacher << endl;
+	cout << "your favor towards enlish is " << favor << endl;
+	cout << "the price of your water bottle is" << price << endl;
+	cout << calc_1 << "+" << calc_2 << "=" << calc_1 + calc_2 << endl;
+	cout << calc_2 << "*" << calc_3 << "=" << calc_2 * calc_3 << endl;
+	cout << calc_3 << "-" << calc_4 << "=" << calc_3 - calc_4 << endl;
+	cout << calc_4 << "/" << calc_1 << "=" << calc_4 / calc_1 << endl;
 	pause();
 }
